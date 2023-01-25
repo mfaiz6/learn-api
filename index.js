03aafcd42d4967fbc96b4f0055950a39715aa814
Mohammad Faiz Ahanger <Mohammadfaiz_2019bcse002@nitsri.net>
2023-01-25 19:26:10 +0530

    add mongoose package and create mongoose connection and create user model schema

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
 
 const app = express()
 
-const PORT = 8800
+dotenv.config()
 
+
+const connect = async () => {
+    try {
+        mongoose.set("strictQuery", false)
+        await mongoose.connect(process.env.MONGO_URL)
+        console.log("Connected to mongod!")
+    } catch (error) {
+        throw error
+    }
+}
+
+mongoose.connection.on("disconnected", () => {
+    console.log("MongoDB down!");
+})
+
+mongoose.connection.on("connected", () => {
+    console.log("MongoDB up & running.");
+})
+
+app.get("/", (req, res) => {
+    res.send("HELLO THERE")
+})
+
+
+
+const PORT = 8800
 app.listen(PORT, () => {
+    connect()
     console.log(`Server started on port ${PORT}`)
 })
